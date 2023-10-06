5f962df9b8cf60ce74578b75781581bc3ffc2012
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-10-06 13:46:36 +0100

    Routng

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import CatalogPage from './pages/CatalogPage';
+import FavoritesPage from './pages/FavoritesPage';
+import Layout from './components/Layout/Layout';
+
+
 function App() {
   return (
-    <div>
-      APP
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />}/>
+          <Route path="/catalog" element={<CatalogPage />}/>
+          <Route path="/favorites" element={<FavoritesPage />}/>
+        </Route>
+      </Routes>
+    </>
   );
 }
 

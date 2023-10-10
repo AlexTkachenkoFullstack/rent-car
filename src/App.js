e9f6d9f6545a97efbb47b87d3a6a650b92c5a364
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-10-10 02:23:54 +0100

    Loader

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
 import FavoritesPage from './pages/FavoritesPage';
 import Layout from './components/Layout';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCars } from './redux/cars/operations';
+import { isCarsLoading } from './redux/cars/selectors';
+import Loader from './components/Loader';
 
 
 function App() {
-  
+  const isCarLoading = useSelector(isCarsLoading);
   const dispatch=useDispatch()
   useEffect(()=>{
   dispatch(fetchCars());
@@ -17,7 +19,9 @@ function App() {
 
   return (
     <>
-      <Routes>
+    {isCarLoading  
+    ? <Loader />
+    : <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />}/>
           <Route path="catalog" element={<CatalogPage />}/>
@@ -25,6 +29,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
+      }
     </>
   );
 }
--- src/redux/cars/selectors.js
+++ src/redux/cars/selectors.js
@@ -1,3 +1,4 @@
 export const getAllCars = state => state.cars.cars;
 export const getFavorites = state => state.cars.favorites;
 export const getFilterCars= state => state.cars.filtredCars;
+export const isCarsLoading= state => state.cars.isLoading;
